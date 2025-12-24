ea9298a30456daa4719a7549066a1ddf9909d48d
Ryan McMahon <2785107M@student.gla.ac.uk>
2025-12-24 16:23:40 +0000

    Updated Reverb to be encapsulated in namespace

--- Source/Effects/Reverb.cpp
+++ Source/Effects/Reverb.cpp
@@ -1,6 +1,7 @@
 #include "Reverb.h"
 
-Reverb::Reverb(float roomSize, float damping, float mix)
+
+audiofx::Reverb::Reverb(float roomSize, float damping, float mix)
     : roomSize(jlimit(0.0f, 1.0f, roomSize)),
     damping(jlimit(0.0f, 1.0f, damping)),
     wetDryMix(jlimit(0.0f, 1.0f, mix))
@@ -10,14 +11,14 @@ Reverb::Reverb(float roomSize, float damping, float mix)
     updateParameters();
 }
 
-void Reverb::prepare(double sampleRate, int samplesPerBlock)
+void audiofx::Reverb::prepare(double sampleRate, int samplesPerBlock)
 {
     currentSampleRate = sampleRate;
     reverbProcessor.reset();
     updateParameters();
 }
 
-void Reverb::process(float* leftChannel, float* rightChannel, int numSamples)
+void audiofx::Reverb::process(float* leftChannel, float* rightChannel, int numSamples)
 {
     if (leftChannel == nullptr || rightChannel == nullptr)
         return;
@@ -26,18 +27,18 @@ void Reverb::process(float* leftChannel, float* rightChannel, int numSamples)
     reverbProcessor.processStereo(leftChannel, rightChannel, numSamples);
 }
 
-String Reverb::getName() const
+String audiofx::Reverb::getName() const
 {
     return "Reverb";
 }
 
-void Reverb::updateParameters()
+void audiofx::Reverb::updateParameters()
 {
     params.roomSize = roomSize;
     params.damping = damping;
     params.wetLevel = wetDryMix;
     params.dryLevel = 1.0f - wetDryMix;
-    params.width = width;
+    params.width = 1.0f;
     params.freezeMode = 0.0f;
 
     reverbProcessor.setParameters(params);
